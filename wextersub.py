47307bc5fe784c61a87f6eb237d24e36514f7da4
wexter777 <107767500+wexter777@users.noreply.github.com>
2022-06-18 19:21:30 +0200

    Add files via upload

---
+++ wextersub.py
@@ -0,0 +1,115 @@
+#ignore the imports 
+import selenium
+from selenium import webdriver
+from os import system, name
+import chromedriver_binary
+from time import time, strftime, gmtime, sleep
+import pyfiglet, os, threading
+import chromedriver_autoinstaller
+import time
+import os
+import sys
+import colorama
+from termcolor import colored
+from time import sleep
+from os import system, name
+from colorama import init, Fore, Back, Style
+import platform
+import psutil
+import random
+import string
+import ctypes
+import ssl
+import requests
+from pystyle import Colorate, Colors, Write, Add, Center
+
+
+ctypes.windll.kernel32.SetConsoleTitleW("Wexter#4806")
+
+def clear():
+
+    if name =='nt':
+        _=system('cls')
+
+    else:
+            _=system("clear")
+
+clear()
+x = input (Colorate.Horizontal(Colors.purple_to_blue, "Username >"))
+if x == "wexter":
+    print(Colorate.Horizontal(Colors.purple_to_blue, "Valid!"))
+    clear()
+else:
+    print(Colorate.Horizontal(Colors.purple_to_blue, "Try again!"))
+    time.sleep(2.0)
+    exit()
+
+x = input (Colorate.Horizontal(Colors.purple_to_blue, "Password >"))
+if x == "wexter":
+    print(Colorate.Horizontal(Colors.purple_to_blue, "Valid"))
+    clear()
+else:
+    print(Colorate.Horizontal(Colors.purple_to_blue, "Try again!"))
+    time.sleep(2.0)
+    exit()
+
+
+clear()
+print(pyfiglet.figlet_format("Wexter", font="slant"))
+
+print("")
+print("")
+print("")
+print("")
+print(colored("                                      (------------)                                      ","blue"))
+print("                                   Loading Wexter Python 0%                               ")
+print("")
+time.sleep(0.5)
+print("")
+print("")
+print("")
+print(colored("                                      (====>-------)                                      ","blue"))
+print("                                   Loading Wexter Python 20%                              ")
+print("")
+time.sleep(0.5)
+print("")
+print("")
+print("")
+print(colored("                                      (========>---)                                      ","blue"))
+print("                                   Loading Wexter Python 75%                              ")
+print("")
+time.sleep(0.5)
+clear()
+
+from pystyle import Colors, Colorate
+print(Colorate.Horizontal(Colors.purple_to_blue, "Made by Wexter#4806", 1))
+time.sleep(2.0)
+clear()
+
+x = input (Colorate.Horizontal(Colors.purple_to_blue, "Subscription >"))
+if x == "123-123":
+    print(Colorate.Horizontal(Colors.purple_to_blue, "Subscription valid!!"))
+else:
+    print(Colorate.Horizontal(Colors.purple_to_blue, "to poor"))
+    exit()
+
+print(Colorate.Horizontal(Colors.purple_to_red, "  ,           ,  ", 1))
+print(Colorate.Horizontal(Colors.purple_to_red, " /             \ ", 1))
+print(Colorate.Horizontal(Colors.purple_to_red, "((__-^^-,-^^-__))", 1))
+print(Colorate.Horizontal(Colors.purple_to_red, " `-_---' `---_-' ", 1))
+print(Colorate.Horizontal(Colors.purple_to_red, "  <__|o` 'o|__>  ", 1))
+print(Colorate.Horizontal(Colors.purple_to_red, "     \  `  /     ", 1))
+print(Colorate.Horizontal(Colors.purple_to_red, "      ): :(      ", 1))
+print(Colorate.Horizontal(Colors.purple_to_red, "      :o_o:      ", 1))
+print(Colorate.Horizontal(Colors.purple_to_red, "                 ", 1))
+time.sleep(1.5)
+
+print("url")
+
+x = input (Colorate.Horizontal(Colors.purple_to_blue, "close? [y/n] >"))
+if x == "y":
+    print("bye")
+    exit()
+else:
+    time.sleep(300.0)
+#Made by Wexter
